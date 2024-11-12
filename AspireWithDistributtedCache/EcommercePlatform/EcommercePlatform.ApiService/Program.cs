068076ace41be1c9bd4ab6c09d5dda7cfedebf71
fiodarsazanavets <f.sazanavets@gmail.com>
2024-11-12 22:21:48 +0000

    Remove redundant code

--- AspireWithDistributtedCache/EcommercePlatform/EcommercePlatform.ApiService/Program.cs
+++ AspireWithDistributtedCache/EcommercePlatform/EcommercePlatform.ApiService/Program.cs
@@ -110,8 +110,3 @@ async Task UpdateProductInDatabaseAsync(int productId, Product updatedProduct)
 app.MapDefaultEndpoints();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
