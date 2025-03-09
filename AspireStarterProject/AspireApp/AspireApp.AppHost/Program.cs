a5731c3ef96461f134ada55a82d9b7c9ac1a0e3d
fiodarsazanavets <f.sazanavets@gmail.com>
2025-03-09 18:34:21 +0000

    Update Aspire to .NET 9 for the basic sample project

--- AspireStarterProject/AspireApp/AspireApp.AppHost/Program.cs
+++ AspireStarterProject/AspireApp/AspireApp.AppHost/Program.cs
@@ -4,6 +4,7 @@ var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice")
 
 builder.AddProject<Projects.AspireApp_Web>("webfrontend")
     .WithExternalHttpEndpoints()
+    .WaitFor(apiService)
     .WithReference(apiService);
 
 builder.Build().Run();
